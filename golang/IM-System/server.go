88c76b61e51909d9ea1830252193a2aa136fb9dc
ryze <ryze2014@live.com>
2021-05-10 23:21:16 +0800

    IM-System by golang

---
+++ golang/IM-System/server.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+	//在线用户列表
+	onlineMap map[string]*User
+	mapLock   sync.RWMutex
+	//消息广播的channel
+	Message chan string
+}
+
+//创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		onlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+//监听Messaged广播消息channel的方法goroutine
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+
+		//将msg发送给全部在线的User
+		this.mapLock.Lock()
+		for _, cli := range this.onlineMap {
+			cli.C <- msg
+
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+//广播消息的方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+
+}
+func (this *Server) Hanlder(conn net.Conn) {
+	//当前连接的业务
+
+	//fmt.Println("连接建立成功")
+	user := NewUser(conn, this)
+
+	//用户上线
+	user.Online()
+
+	//接手客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				user.Offline()
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+			//将用户消息提取（去除'\n'）
+			msg := string(buf[:n-1])
+
+			//用户针对msg进行消息处理
+			user.DoMessage(msg)
+		}
+	}()
+
+	//当前handler阻塞
+	select {}
+}
+
+//启动服务器的接口
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err :", err)
+	}
+	//close listen socket
+	defer listener.Close()
+
+	//启动监听Message的goroutine
+	go this.ListenMessage()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listen accept err:", err)
+			continue
+		}
+		//do handler
+		go this.Hanlder(conn)
+	}
+
+}
---
+++ golang/IM-System/user.go
@@ -0,0 +1,64 @@
+package main
+
+import "net"
+
+type User struct {
+	Name   string
+	Addr   string
+	C      chan string
+	conn   net.Conn
+	server *Server
+}
+
+//创建一个用户的API
+func NewUser(conn net.Conn, server *Server) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+
+		server: server,
+	}
+	//启动监听当前user channel消息的goroutine
+	go user.ListenMessage()
+
+	return user
+}
+
+//用户上线的业务
+func (this *User) Online() {
+	//用户上线，将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.onlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.server.BroadCast(this, "已上线")
+}
+
+//用户下线的任务
+func (this *User) Offline() {
+
+	//用户下线，将用户从onlineMap中删除
+	this.server.mapLock.Lock()
+	delete(this.server.onlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	//广播当前用户下线消息
+	this.server.BroadCast(this, "下线")
+}
+
+//处理用户下线的业务
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
+}
+
+//监听当前的User Channel的方法，一旦有消息，就直接发送给对端客户端
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+		this.conn.Write([]byte(msg + "\n"))
+	}
+}
